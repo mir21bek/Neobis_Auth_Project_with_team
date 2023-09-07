eb9b77e8a5ee40712af1d7f94672541cfd8494ed
Azat <azatss442@gmail.com>
2023-09-07 11:56:55 +0600

    fixing bug in viewes

--- accounts/views.py
+++ accounts/views.py
@@ -1,41 +1,37 @@
 from django.contrib.auth import login
 from django.contrib.auth import get_user_model
-
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from .serializers import CustomUserSerializer
 from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
 User = get_user_model()
 
 
 class UserProfileViewSet(generics.ListCreateAPIView):
-    queryset = User.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [permissions.AllowAny]
 
     def get_queryset(self):
-        if self.request.user.is_active:
-            return User.objects.filter(is_active=True)
-        return User.objects.none()
-
-
+        return User.objects.filter(is_superuser=False)
 class ConfirmEmailView(generics.GenericAPIView):
     @staticmethod
     def get(request, token):
         try:
             user = User.objects.get(token_auth=token)
+            if user.is_active:
+                return Response({'detail': 'User is already activated'}, status=status.HTTP_200_OK)
+
             user.is_active = True
             user.save()
 
             login(request, user)
             refresh = RefreshToken.for_user(user)
             return Response({
+                'detail': 'Email confirmation successful',
                 'refresh': str(refresh),
                 'access': str(refresh.access_token),
             }, status=status.HTTP_200_OK)
         except User.DoesNotExist:
-            return Response({'detail': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
-
-
+            return Response({'detail': 'Invalid token'}, status=status.HTTP_404_NOT_FOUND)
